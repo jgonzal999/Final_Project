bfda08dda07c6a9c5b4d3aeb7d1904c26936a96f
Javier <jgonzal99@gmail.com>
2023-09-02 09:48:02 +0000

    Final-9

---
+++ Final_Project_App/src/enhanced/UsersE.java
@@ -0,0 +1,14 @@
+package enhanced;
+
+import java.util.ArrayList;
+import java.util.Hashtable;;
+
+public class UsersE {
+	private String login = new String();
+	private String pwd = new String();
+	private String name = new String();
+	
+	
+	
+
+}
